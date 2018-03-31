05b3807bd699566d24119c2600656248d40785c7
pmgandh2 <pmgandh2@ncsu.edu>
2018-03-31 15:54:13 -0400

    added for functionalities

--- src/Application.java
+++ src/Application.java
@@ -5,6 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+
 public class Application {
 	
 	Connection connection;
@@ -99,7 +100,24 @@ public class Application {
 			case 1:
 				addHotel();
 				break;
-			//case 2:
+			case 2:
+				getHotelInfoByID();
+				break;
+			case 3:
+				updateHotel();
+				break;
+			case 4:
+				deleteHotel();
+				break;
+			case 5:
+				addRoom();
+				break;
+			case 6:
+				updateRoom();
+			case 8:
+				addStaff();
+				break;
+				
 			default:
 				this.prompt("f");
 				
@@ -108,6 +126,189 @@ public class Application {
 		
 	}
 	
+	public void updateRoom() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+
+	public void addRoom() throws SQLException{
+		// TODO Auto-generated method stub
+		String name, address, phone, dept, password;
+		//int  managerid=(Integer) null;		
+		//check if this manager id is already present in staff table	
+		int roomNumber,hotelid, maxOccupancy,rate;
+		boolean availability;
+		String serviceDesc, category;
+		prompt("Enter room number"); roomNumber = this.scan.nextInt();
+		prompt("Enter hotelID"); hotelid = this.scan.nextInt();
+		prompt("enter service desc"); serviceDesc = this.scan.nextLine();
+		prompt("enter max occupancy"); maxOccupancy = this.scan.nextInt();
+		prompt("Enter category"); category = scan.next();
+		prompt("Enter availability"); availability = scan.nextBoolean();
+		prompt("Enter rate"); rate = scan.nextInt();
+		//prompt("Enter address"); address = scan.next();
+		//prompt("Enter phone"); phone = scan.next();
+		
+		String sql = "Insert Into ROOM Values(?,?,?,?,?,?,?)";
+		
+		
+		
+//		ps.setInt(7, roomNumber);
+//		ps.setInt(8, roomNumber);
+		
+		
+		try {
+			PreparedStatement ps=connection.prepareStatement(sql);			
+			ps.setInt(1, roomNumber);
+			ps.setInt(2, hotelid);
+			ps.setString(3, serviceDesc);
+			ps.setInt(4, maxOccupancy);
+			ps.setString(5, category);
+			ps.setBoolean(6, availability);
+			ps.setInt(7, rate);
+			ps.executeQuery();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}			
+	}
+
+
+	public void getHotelInfoByID() throws SQLException {
+		// TODO Auto-generated method stub
+		prompt("please enter the hotel id");
+		int hotelid = this.scan.nextInt();
+		String sql = "select * from HOTEL where HotelID=?";
+		PreparedStatement ps = this.connection.prepareStatement(sql);
+		ps.setInt(1, hotelid);		
+		ResultSet rs= ps.executeQuery();
+		while(rs.next()) {
+			prompt("hotelid="+rs.getInt(1)+" managerid="+rs.getInt(2)+" name="+rs.getString(3)
+					+ " address="+rs.getString(4)+" city="+rs.getString(5)+" state="+rs.getString(6)
+					+ " email="+rs.getString(7)+" phone="+rs.getString(8));
+		}
+	}
+
+
+	public void deleteHotel() throws SQLException{
+		// TODO Auto-generated method stub
+		prompt("please enter the hotel id");
+		int hotelid = this.scan.nextInt();
+		String sql = "delete from HOTEL where HotelID=?";
+		PreparedStatement ps = this.connection.prepareStatement(sql);
+		ps.setInt(1, hotelid);
+		ps.executeQuery();	
+	}
+
+
+	public void updateHotel() throws SQLException{
+		// TODO Auto-generated method stub
+		prompt("enter hotel id");
+		int hotelid = scan.nextInt();
+		prompt("please select the field to be updated");
+		prompt("1. ManagerID 2.Name 3.Address 4.City 5.State 6.email 7.phone");
+		String sql = "select * from Hotel where hotelid=?";
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ps.setInt(1, hotelid);
+		ResultSet rs = ps.executeQuery();
+		int manageridfetched = 0;
+		String namefetched="", addressfetched="", cityfetched="", statefetched="", emailfetched="", phonefetched="";
+		while(rs.next()) {
+			manageridfetched = rs.getInt(2);
+			namefetched = rs.getString(3);
+			addressfetched = rs.getString(4);
+			statefetched = rs.getString(5);
+			emailfetched = rs.getString(6);
+			phonefetched  =rs.getString(7);
+			break;
+		}
+		
+		int choice = this.scan.nextInt();
+		switch (choice) {
+		case 1:
+			prompt("enter manager id:");		
+			int managerid = scan.nextInt();
+			UpdateHotelHelper(hotelid, managerid, namefetched, addressfetched, cityfetched, statefetched, emailfetched, phonefetched);
+			break;
+		case 2: 
+			prompt("enter name");
+			String name = scan.next();
+			UpdateHotelHelper(hotelid, manageridfetched, name, addressfetched, cityfetched, statefetched, emailfetched, phonefetched);
+			break;
+		case 3:
+			prompt("enter address");
+			String address = scan.next();
+			UpdateHotelHelper(hotelid, manageridfetched, namefetched, address, cityfetched, statefetched, emailfetched, phonefetched);			
+			break;
+		case 4: 
+			prompt("enter city");
+			String city = scan.next();
+			UpdateHotelHelper(hotelid, manageridfetched, namefetched, addressfetched, city, statefetched, emailfetched, phonefetched);
+			break;
+		case 5: 
+			prompt("enter state");
+			String state = scan.next();
+			UpdateHotelHelper(hotelid, manageridfetched, namefetched, addressfetched, cityfetched, state, emailfetched, phonefetched);
+			break;
+		case 6: 
+			prompt("enter email");
+			String email = scan.next();
+			UpdateHotelHelper(hotelid, manageridfetched, namefetched, addressfetched, cityfetched, statefetched, email, phonefetched);
+			break;
+		case 7: 
+			prompt("enter phone");
+			String phone = scan.next();
+			UpdateHotelHelper(hotelid, manageridfetched, namefetched, addressfetched, cityfetched, statefetched, emailfetched, phone);
+			break;
+		default:
+			prompt("invalid option");
+			break;
+		}
+
+	}
+	 
+
+
+	public void addStaff() throws SQLException{
+		// TODO Auto-generated method stub
+		String name, address, phone, dept, password;
+		//int  managerid=(Integer) null;		
+		//check if this manager id is already present in staff table	
+		int hotelid, age;
+		boolean availability,isactive;
+		
+		prompt("Enter hotelID"); hotelid = scan.nextInt();
+		prompt("Enter name"); name = scan.next();
+		prompt("Enter age"); age = scan.nextInt();
+		prompt("Enter address"); address = scan.next();
+		prompt("Enter phone"); phone = scan.next();
+		prompt("Enter availability"); availability = scan.nextBoolean();
+		prompt("Enter dept"); dept = scan.next();
+		prompt("Enter isactive"); isactive = scan.nextBoolean();
+		prompt("Enter password"); password = scan.next();
+		String sql = "INSERT into HOTEL(Name,Address,City,State,Email,Phone) values (?,?,?,?,?,?)";
+		try {
+			PreparedStatement ps = this.connection.prepareStatement(sql);
+			
+			ps.setInt(1, hotelid);
+			ps.setString(2, name);
+			ps.setInt(3, age);
+			ps.setString(4, address);
+			ps.setString(5, phone);
+			ps.setBoolean(6, availability);
+			ps.setString(7, dept);
+			ps.setBoolean(8, isactive);
+			ps.setString(9, password);			
+			ps.executeQuery();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}			
+	}
+
+
 	public void addHotel() throws SQLException {
 		// TODO Auto-generated method stub
 		String name, address, city, state, email, phone;
@@ -134,15 +335,21 @@ public class Application {
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
-		}			
+		}	
+		
+		prompt("now you need to add this manager to staff table");
+		addStaff();
+		adminHandler();
 
 	}
+	
+	
 
 
 	public Boolean isStaff(int managerid) throws SQLException {
 		// TODO Auto-generated method stub
 		String sql = "select * from Staff where staffID =?";
-		PreparedStatement statement = connection.prepareStatement(sql);
+		PreparedStatement statement = this.connection.prepareStatement(sql);
         statement.setInt(1, managerid);
         //statement.setString(2, userPassword);
         ResultSet resultSet = statement.executeQuery();
@@ -207,6 +414,25 @@ public class Application {
         }
         return valid;
     }
+	
+	 
+
+	
+
+
+	public void UpdateHotelHelper(int hotelid,int managerid, String name, String address, String city, String state, String email, String phone) throws SQLException {
+		String sql = "update HOTEL SET ManagerID=?, Name=?, Address=?, City=?, State=?, Email=?, Phone=? where HotelID=?";
+		PreparedStatement ps = this.connection.prepareStatement(sql);
+		ps.setInt(1, managerid);
+		ps.setString(2, name);
+		ps.setString(3,  address);
+		ps.setString(4, city);
+		ps.setString(5, state);
+		ps.setString(6, email);
+		ps.setString(7, phone);		 
+		ps.setInt(8, hotelid);
+		ps.executeQuery();
+	 }
 
 
 	public static void main(String[] args) {
@@ -243,7 +469,7 @@ public class Application {
         }
         scan.close();
     }
-
+	
 
 	
 
